ddf598aa299182ec87d35d28ed3fd351a539c274
dimensionalchaos <90750149+dimensionalchaos@users.noreply.github.com>
2023-04-17 13:21:19 +0530

    added updated game files

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 import pygame as pg
 import os
 import math
+import random
 
 pg.init()
-screen = pg.display.set_mode()
-
+screen = pg.display.set_mode((960,400))
 clock = pg.time.Clock()
 split = os.path.split(os.path.abspath(__file__))[0]
 maindir = os.path.join(split, "mygamedata")
@@ -26,11 +27,11 @@ class Pscore(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.p = "score" + str(ball.count) + ".png"
-        self.image, self.rect = load_image(self.p, (200, 200), -1)
+        self.image, self.rect = load_image(self.p, (80, 80), -1)
 
     def update(self):
         self.p = "score" + str(ball.count) + ".png"
-        self.image, self.rect = load_image(self.p, (200, 200), -1)
+        self.image, self.rect = load_image(self.p, (80, 80), -1)
 
 
 class Bscore(pg.sprite.Sprite):
@@ -38,28 +39,28 @@ class Bscore(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.p = "score" + str(ball.count2) + ".png"
-        self.image, self.rect = load_image(self.p, (200, 200), -1)
-        self.rect.right = (screen.get_width() - 50)
-        self.rect.top = (screen.get_height() - 50)
+        self.image, self.rect = load_image(self.p, (80, 80), -1)
+        self.rect.right = (screen.get_width()/2)
+        self.rect.top = (screen.get_height()/2)
 
     def update(self):
         self.p = "score" + str(ball.count2) + ".png"
-        self.image, self.rect = load_image(self.p, (200, 200), -1)
-        self.rect.right = (screen.get_width() - 50)
-        self.rect.top = (screen.get_height() - 860)
+        self.image, self.rect = load_image(self.p, (80, 80), -1)
+        self.rect.right = (screen.get_width()/2+480)
+        self.rect.top = (screen.get_height()/2+-195)
 
 
 class Ball(pg.sprite.Sprite):
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image("ball.png",(50,50), -1)
-        self.rect.y = 1
-        self.rect.x = 1000
+        self.image, self.rect = load_image("ball.png",(25,25), -1)
+        self.rect.y = screen.get_height()/2
+        self.rect.x = screen.get_width()/2
         self.original = self.rect
         self.image = self.image.convert()
-        self.xspeed = -20
-        self.yspeed = -15
+        self.xspeed = 7
+        self.yspeed = 8
         self.count = 0
         self.count2 = 0
 
@@ -67,15 +68,16 @@ class Ball(pg.sprite.Sprite):
         if (self.rect.top < screen.get_rect().top
                 or self.rect.bottom > screen.get_rect().bottom):
             self.yspeed = -self.yspeed
-        if self.rect.colliderect(paddle.rect) or self.rect.colliderect(
-                paddle2.rect):
+        if self.rect.colliderect(paddle.rect) or self.rect.colliderect(paddle2.rect):
+            print("collided")
+            print(screen.get_size())
             self.xspeed = -self.xspeed
         if self.rect.left < screen.get_rect().left:
-            self.rect = self.original
             self.count2 = self.count2 + 1
-        if self.rect.right > screen.get_rect().right:
             self.rect = self.original
+        if self.rect.right > screen.get_rect().right:
             self.count = self.count + 1
+            self.rect = self.original
         newpos = self.rect.move((self.xspeed, self.yspeed))
         self.rect = newpos
 
@@ -84,9 +86,9 @@ class Paddle(pg.sprite.Sprite):
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image("paddle.png",(20,200))
+        self.image, self.rect = load_image("paddle.png",(10,55))
         self.image = self.image.convert()
-        self.speed = 20
+        self.speed = 15
         self.up = False
         self.down = False
 
@@ -120,21 +122,33 @@ class Paddle2(pg.sprite.Sprite):
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image("paddle.png",(20, 200))
+        self.image, self.rect = load_image("paddle.png",(10,55))
         self.image = self.image.convert()
         self.rect.right = screen.get_rect().right
         self.rect.midright = screen.get_rect().midright
         self.ballup = False
         self.balldown = False
-        self.speed = 20
-
+        self.speed = 15
+        self.failure = None
     def update(self):
+        
         if self.ballup == True:
+            self.failure = random.randrange(0,1000)
             newpos = self.rect.move((0, -self.speed))
+            if self.failure == 1:
+                newpos = self.rect.move((0,4))
             self.rect = newpos
+            if self.rect.top<screen.get_rect().top:
+                self.rect.top = screen.get_rect().top
+
         if self.balldown == True:
+            self.failure = random.randrange(0,2)
             newpos = self.rect.move((0, self.speed))
+            if self.failure == 1:
+                newpos = self.rect.move((0,-4))
             self.rect = newpos
+            if self.rect.bottom>screen.get_rect().bottom:
+                self.rect.bottom = screen.get_rect().bottom
 
 
 paddle = Paddle()
@@ -144,47 +158,46 @@ pscore = Pscore()
 bscore = Bscore()
 screen.fill("green")
 sprites = pg.sprite.RenderPlain((paddle, pscore, paddle2, bscore, ball))
-
 while True:
-    clock.tick(60)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-        if event.type == pg.KEYDOWN and event.key == pg.K_s:
-            paddle.down = True
-        elif event.type == pg.KEYDOWN and event.key == pg.K_w:
-            paddle.up = True
-        elif event.type == pg.KEYUP and event.key == pg.K_w:
-            paddle.uplifted()
-        elif event.type == pg.KEYUP and event.key == pg.K_s:
-            paddle.downlifted()
-    if abs(ball.rect.right - paddle2.rect.left) <= 1000:
-        if (ball.rect.bottom < paddle2.rect.top):
-            paddle2.ballup = True
+        clock.tick(60)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            if event.type == pg.KEYDOWN and event.key == pg.K_s:
+                paddle.down = True
+            elif event.type == pg.KEYDOWN and event.key == pg.K_w:
+                paddle.up = True
+            elif event.type == pg.KEYUP and event.key == pg.K_w:
+                paddle.uplifted()
+            elif event.type == pg.KEYUP and event.key == pg.K_s:
+                paddle.downlifted()
+        if abs(ball.rect.right - paddle2.rect.left) <= 200:
+            if (ball.rect.bottom < paddle2.rect.top):
+                paddle2.ballup = True
+                paddle2.balldown = False
+            elif (ball.rect.top > paddle2.rect.top):
+                paddle2.balldown = True
+                paddle2.ballup = False
+        else:
             paddle2.balldown = False
-        elif (ball.rect.top > paddle2.rect.top):
-            paddle2.balldown = True
             paddle2.ballup = False
-    else:
-        paddle2.balldown = False
-        paddle2.ballup = False
-    sprites.update()
-    screen.fill("green")
-    sprites.draw(screen)
-    pg.display.flip()
-    if (ball.count == 5 or ball.count2 == 5):
-        overdir = os.path.join(maindir, "gameover.png")
-        windir = os.path.join(maindir, "youwin.png")
-        if (ball.count2 == 5):
-            over = pg.image.load(overdir)
-        elif (ball.count == 5):
-            over = pg.image.load(windir)
-        over = pg.transform.scale(over, (screen.get_size()))
-        over = over.convert()
-        while True:
-            screen.blit(over, (0, 0))
-            clock.tick(60)
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-            pg.display.flip()
+        sprites.update()
+        screen.fill("green")
+        sprites.draw(screen)
+        pg.display.flip()
+        if (ball.count == 5 or ball.count2 == 5):
+            overdir = os.path.join(maindir, "gameover.png")
+            windir = os.path.join(maindir, "youwin.png")
+            if (ball.count2 == 5):
+                over = pg.image.load(overdir)
+            elif (ball.count == 5):
+                over = pg.image.load(windir)
+            over = pg.transform.scale(over, (screen.get_size()))
+            over = over.convert()
+            while True:
+                screen.blit(over, (0, 0))
+                clock.tick(60)
+                for event in pg.event.get():
+                    if event.type == pg.QUIT:
+                        pg.quit()
+                pg.display.flip()
